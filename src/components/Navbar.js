f2f7d50b9999ece984e1aa6c7b323cf52d1879d3
Shunmuka Valsa <shunmukavalsa@gmail.com>
2025-04-14 15:34:53 -0400

    Edit and delete and forgot password

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css'; // Optional styling
+import React, { useState } from 'react';
+import { signOut } from 'firebase/auth';
+import { auth } from '../FireBase'; 
+import { useNavigate, Link } from 'react-router-dom';
+import './Navbar.css';
 
-const Navbar = () => {
+function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      navigate('/'); 
+    } catch (error) {
+      console.error('Error signing out:', error);
+    }
+  };
+
   return (
     <nav className="navbar">
-      <ul className="navbar-list">
-        <li><Link to="/home">Internships</Link></li>
-        <li><Link to="/account">Account</Link></li>
-        <li><Link to="/job-board">Job Board</Link></li>
-      </ul>
+      <div className="navbar-title">Internship Tracker</div>
+      <div className="navbar-links">
+        <Link to = "/home">Internships</Link>
+        <Link to="/job-board" className="job-board-link">Job Board</Link>
+        <a href="#logout" onClick={handleLogout}>Log Out</a>
+      </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
-    
